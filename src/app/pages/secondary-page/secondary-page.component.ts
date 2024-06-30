404689ba89960528690ccee3b72fffc1e2c26385
Diego Sánchez <dsv.9cc@gmail.com>
2024-06-30 18:00:16 +0200

    chore: Remove unused import in secondary-page.component.ts

--- src/app/pages/secondary-page/secondary-page.component.ts
+++ src/app/pages/secondary-page/secondary-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
